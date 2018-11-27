cd7fac7d6eacde205fe6aa52a31b346a489a25ca
Jessica Mccormack <jesslynn.mack@gmail.com>
2018-11-26 19:36:48 -0500

    modified server.js for heroku deployment

--- server.js
+++ server.js
@@ -9,8 +9,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+var databaseUri = 'mongodb://localhost/kudos_db';
+if (process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGODB_URI);
+}else {
+  mongoose.connect(databaseUri);
+}
 
-mongoose.connect('mongodb://localhost/kudos_db', { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', function(err){
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function(){
+  console.log('Mongoose Connection Successful.')
+});
 
 
 require('./routes/api-routes')(app);
